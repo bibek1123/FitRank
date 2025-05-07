50dfe6f8046f749b9058842d298fbe1ba46601c4
bibek1123 <bibekece7@gmail.com>
2025-05-07 18:33:43 +0530

    seeder (activity_time) hard coded remove

--- src/seeders/activitiesSeeder.js
+++ src/seeders/activitiesSeeder.js
@@ -4,16 +4,16 @@ import logger from '../middlewares/logger.js'
 export async function seedActivities() {
 
   const activities = [
-    { user_id: 1, activity_time: new Date('2025-05-06T08:00:00'), points: 20 },
-    { user_id: 1, activity_time: new Date('2025-05-06T09:00:00'), points: 20 },
-    { user_id: 2, activity_time: new Date('2025-05-06T10:00:00'), points: 20 },
-    { user_id: 2, activity_time: new Date('2025-05-06T11:00:00'), points: 20 },
-    { user_id: 3, activity_time: new Date('2025-05-06T12:00:00'), points: 20 },
-    { user_id: 3, activity_time: new Date('2025-05-06T13:00:00'), points: 20 },
-    { user_id: 4, activity_time: new Date('2025-05-06T14:00:00'), points: 20 },
-    { user_id: 4, activity_time: new Date('2025-05-06T15:00:00'), points: 20 },
-    { user_id: 5, activity_time: new Date('2025-05-06T16:00:00'), points: 20 },
-    { user_id: 5, activity_time: new Date('2025-05-06T17:00:00'), points: 20 },
+    { user_id: 1, activity_time: new Date(), points: 20 },
+    { user_id: 1, activity_time: new Date(), points: 20 },
+    { user_id: 2, activity_time: new Date(), points: 20 },
+    { user_id: 2, activity_time: new Date(), points: 20 },
+    { user_id: 3, activity_time: new Date(), points: 20 },
+    { user_id: 3, activity_time: new Date(), points: 20 },
+    { user_id: 4, activity_time: new Date(), points: 20 },
+    { user_id: 4, activity_time: new Date(), points: 20 },
+    { user_id: 5, activity_time: new Date(), points: 20 },
+    { user_id: 5, activity_time: new Date(), points: 20 },
   ];
 
   await Activity.bulkCreate(activities);
